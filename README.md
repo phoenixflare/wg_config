7fd2856b9206ce42e1087472da6ddac915658f9c
Al <github@phoenixflare.net>
2020-09-01 18:56:20 -0700

    Added more functionality to the package. - Removed sample def file. - Added init script to generate wg.def. - Added config script to manage wireguard configuration. - Added option for whitelisting and blacklisting LAN addresses.

--- README.md
+++ README.md
@@ -15,27 +15,27 @@ Client config file and qrcode are generated.
 
 
 
-### dependency
+### Dependency
 
 * wireguard
 * qrencode
 
-### config
+### Config
 The wireguard default config directory is /etc/wireguard.
 The script config file is wg.def, create and edit it according to wg.def.sample.
 You can generate the public key and private key with command `wg genkey | tee > prikey | wg pubkey > pubkey`.
 
-### usage
+### Usage
 
 Running as root.
 
-#### start wireguard
+#### Start wireguard
 
 ```bash
-wg-quick up wg0
+./init.sh
 ```
 
-#### add a user
+#### Add a user
 
 ```bash
 ./user.sh -a alice
@@ -44,14 +44,14 @@ wg-quick up wg0
 This will generate a client conf and qrcode in current directory which name is alice
 and add alice to the wg config.
 
-#### delete a user
+#### Delete a user
 
 ```bash
 ./user.sh -d alice
 ```
 This will delete the alice directory and delete alice from the wg config.
 
-#### view a user
+#### View a user
 
 ```bash
 ./user.sh -v alice
@@ -59,8 +59,47 @@ This will delete the alice directory and delete alice from the wg config.
 This will show generated QR codes.
 
 
-#### clear all
+#### Clear all
 
 ```bash
-./user.sh -c
+./config.sh -d
+```
+
+
+#### Reload wireguard configuration
+```bash
+./config.sh -r
+```
+
+
+###### Packet forwarding and ip address limiting
+
+By default, data from wireguard peers cannot access the LAN.
+To allow peers to access the LAN, uncomment the following line in */etc/syscl.conf*
+```bash
+net.ipv4.ip_forward
+```
+A restart is required after his has been enabled.
+
+
+### Whitelist and blacklist
+The default iptable rules for wireguard will allow all traffic from the peers to the LAN.
+If you wish to limit access to certain addresses, add the LAN range to the *server.conf.blacklist*.
+IP addresses added to *server.conf.whitelist* will be forward from the peers.
+
+The following will only allow 192.168.1.100 and 192.168.1.48 and prevent any other access to the LAN
+# server.conf.whitelist
+```bash
+192.168.1.48/32
+192.168.1.100/32
+```
+
+# server.conf.blacklist
+```bash
+192.168.1.0/24
+```
+
+When these files are changed, a reload of the wireguard interface is required.
+```bash
+./config.sh -r
 ```
--- user.sh
+++ user.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cd `dirname ${BASH_SOURCE[0]}`
 
 . wg.def
 CLIENT_TPL_FILE=client.conf.tpl
-SERVER_TPL_FILE=server.conf.tpl
 SAVED_FILE=.saved
 AVAILABLE_IP_FILE=.available_ip
-WG_TMP_CONF_FILE=.$_INTERFACE.conf
 WG_CONF_FILE="/etc/wireguard/$_INTERFACE.conf"
 
 dec2ip() {
@@ -41,73 +39,71 @@ generate_cidr_ip_file_if() {
     local i=$((beg+2))
     while [[ $i -lt $end ]]; do
         ip=$(dec2ip $i)
-	echo "$ip/$mask" >> $AVAILABLE_IP_FILE
+        echo "$ip/$mask" >> $AVAILABLE_IP_FILE
         i=$((i+1))
     done
 }
 
+
 get_vpn_ip() {
     local ip=$(head -1 $AVAILABLE_IP_FILE)
     if [[ $ip ]]; then
-	local mat="${ip/\//\\\/}"
+        local mat="${ip/\//\\\/}"
         sed -i "/^$mat$/d" $AVAILABLE_IP_FILE
     fi
     echo "$ip"
 }
 
 add_user() {
-    local user=$1
-    local template_file=${CLIENT_TPL_FILE}
-    local interface=${_INTERFACE}
-    local userdir="users/$user"
+	local user=$1
+	local template_file=${CLIENT_TPL_FILE}
+	local interface=${_INTERFACE}
+	local userdir="users/$user"
 
-    if [ ! -d "$userdir" ]
-    then
+	if [ ! -d "$userdir" ] ; then
+		mkdir -p "$userdir"
+		wg genkey | tee $userdir/privatekey | wg pubkey > $userdir/publickey
 
-     mkdir -p "$userdir"
-     wg genkey | tee $userdir/privatekey | wg pubkey > $userdir/publickey
+		# client config file
+		_PRIVATE_KEY=`cat $userdir/privatekey`
+		_VPN_IP=$(get_vpn_ip)
+		if [[ -z $_VPN_IP ]]; then
+			echo "no available ip"
+			exit 1
+		fi
+		eval "echo \"$(cat "${template_file}")\"" > $userdir/client.conf
 
-     # client config file
-     _PRIVATE_KEY=`cat $userdir/privatekey`
-     _VPN_IP=$(get_vpn_ip)
-     if [[ -z $_VPN_IP ]]; then
-         echo "no available ip"
-         exit 1
-     fi
-     eval "echo \"$(cat "${template_file}")\"" > $userdir/client.conf
-     
-     eval "echo \"$(cat "${template_file}")\"" > $userdir/client.all.conf
-     sed -r "s/AllowedIPs.*/AllowedIPs = 0.0.0.0\/0/g" -i $userdir/client.all.conf
-     
-     qrencode -t ansiutf8  < $userdir/client.conf
-     qrencode -o $userdir/$user.png  < $userdir/client.conf
+		eval "echo \"$(cat "${template_file}")\"" > $userdir/client.all.conf
+		sed -r "s/AllowedIPs.*/AllowedIPs = 0.0.0.0\/0/g" -i $userdir/client.all.conf
 
-     qrencode -o $userdir/$user.all.png  < $userdir/client.all.conf
-     
-     # change wg config
-     local ip=${_VPN_IP%/*}/32
-     local public_key=`cat $userdir/publickey`
-     wg set $interface peer $public_key allowed-ips $ip
-     if [[ $? -ne 0 ]]; then
-       echo "wg set failed"
-       rm -rf $user
-       exit 1
-     fi
+		qrencode -t ansiutf8  < $userdir/client.conf
+		qrencode -o $userdir/$user.png  < $userdir/client.conf
 
-     echo "$user $_VPN_IP $public_key" >> ${SAVED_FILE}
-    
-    else
-     echo "$user already exists." 1>&2
-     echo
-     read -r -p "Overwrite current user? [y/N]" response
-     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-     then
-        del_user $user
-        add_user $user
-     else
-        echo "Exiting."
-     fi
-    fi
+		qrencode -o $userdir/$user.all.png  < $userdir/client.all.conf
+
+		# change wg config
+		local ip=${_VPN_IP%/*}/32
+		local public_key=`cat $userdir/publickey`
+		wg set $interface peer $public_key allowed-ips $ip
+		if [[ $? -ne 0 ]]; then
+			echo "wg set failed"
+			rm -rf $user
+			exit 1
+		fi
+
+		echo "$user $_VPN_IP $public_key" >> ${SAVED_FILE}
+
+		else
+		echo "$user already exists." 1>&2
+		echo
+		read -r -p "Overwrite current user? [y/N]" response
+		if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] ; then
+			del_user $user
+			add_user $user
+		else
+			echo "Exiting."
+		fi
+	fi
 }
 
 del_user() {
@@ -129,34 +125,9 @@ del_user() {
         echo "$ip" >> ${AVAILABLE_IP_FILE}
     fi
     rm -rf $userdir
-    
+
     sort ${AVAILABLE_IP_FILE} --version-sort -o ${AVAILABLE_IP_FILE}
 }
-
-generate_and_install_server_config_file() {
-    local template_file=${SERVER_TPL_FILE}
-    local ip
-
-    # server config file
-    eval "echo \"$(cat "${template_file}")\"" > $WG_TMP_CONF_FILE
-    while read user vpn_ip public_key; do
-      ip=${vpn_ip%/*}/32
-      cat >> $WG_TMP_CONF_FILE <<EOF
-[Peer]
-PublicKey = $public_key
-AllowedIPs = $ip
-EOF
-    done < ${SAVED_FILE}
-    \cp -f $WG_TMP_CONF_FILE $WG_CONF_FILE
-}
-
-do_clear() {
-    local interface=$_INTERFACE
-    wg-quick down $interface
-    > $WG_CONF_FILE
-    rm -f ${SAVED_FILE} ${AVAILABLE_IP_FILE}
-}
-
 do_user() {
     generate_cidr_ip_file_if
 
@@ -170,7 +141,7 @@ do_user() {
         del_user $user
     fi
 
-    generate_and_install_server_config_file
+    ./config.sh -r
 }
 
 view_user() {
@@ -203,7 +174,6 @@ usage() {
     echo
     echo "       -a [username]                              add user"
     echo "       -d [username]                           delete user"
-    echo "       -c               delete all users and configuration"
     echo "       -v [username]      view generated QR codes for user"
     echo
 
@@ -218,16 +188,8 @@ fi
 action=$1
 user=$2
 
-if [[ $action == "-c" ]]; then
-    read -r -p "Deleting all users and config. Are you sure? [y/N] " response
-    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-    then
-        do_clear
-    else
-        echo "Exiting."
-    fi
-elif [[ $action == "-v" ]]; then
-    view_user $user   
+if [[ $action == "-v" ]]; then
+    view_user $user
 elif [[ $action == "-g" ]]; then
     generate_cidr_ip_file_if
 elif [[ ! -z "$user" && ( $action == "-a" || $action == "-d" ) ]]; then
@@ -236,3 +198,4 @@ else
     usage
     exit 1
 fi
+
